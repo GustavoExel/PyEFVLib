18e281574f4d62905233f707f726b1c1cb8318ec
gustavoexelgpe@gmail.com <gustavoexelgpe@gmail.com>
2020-05-30 12:51:19 -0300

    stress_equilibrium.py and CgnsSaver fixes

--- apps/stress_equilibrium.py
+++ apps/stress_equilibrium.py
@@ -12,7 +12,7 @@ grid = Grid(reader.getData())
 problemData.grid = grid
 problemData.read()
 
-cgnsSaver = CgnsSaver(grid, problemData.paths["Output"], problemData.libraryPath, ['u', 'v'])
+cgnsSaver = CgnsSaver(grid, problemData.paths["Output"], problemData.libraryPath)
 
 currentTime = 0.0
 numberOfVertices = grid.vertices.size
@@ -26,14 +26,18 @@ converged = False
 
 print("{:>9}\t{:>14}\t{:>14}\t{:>14}".format("Iteration", "CurrentTime", "TimeStep", "Difference"))
 
-def computeLocalMatrixCoefficient(innerFace, shearModulus, lamesConstant):
-	area=innerFace.area.getCoordinates()
-	transposedVoigtArea = np.array([[area[0], 0, area[1]],[0, area[1], area[0]]])
-	# coefficient = np.zeros(2,2,innerFace.element.vertices.size)
-
-	Nx, Ny = innerFace.globalDerivatives
+def computeLocalMatrixCoefficient(innerFace, G, poissonsRatio):
 	Sx, Sy, Sz = innerFace.area.getCoordinates()
-	return np.array([[(2*shearModulus+lamesConstant)*Sx*Nx+shearModulus*Sx*Ny, lamesConstant*Sx*Ny+shearModulus*Sx*Nx],[lamesConstant*Sy*Ny+shearModulus*Sy*Nx, (2*shearModulus+lamesConstant)*Sy*Ny+shearModulus*Sy*Nx]])
+	l=2*shearModulus*poissonsRatio/(1-poissonsRatio)
+	b=l*(-1+1/poissonsRatio)
+
+	coefficient = np.zeros([2,2,innerFace.element.vertices.size])
+	coefficient[0][0] = np.matmul( np.array([ Sx*b, Sy*G ]), innerFace.globalDerivatives )
+	coefficient[0][1] = np.matmul( np.array([ Sy*G, Sx*l ]), innerFace.globalDerivatives )
+	coefficient[1][0] = np.matmul( np.array([ Sy*l, Sx*G ]), innerFace.globalDerivatives )
+	coefficient[1][1] = np.matmul( np.array([ Sx*G, Sy*b ]), innerFace.globalDerivatives )
+
+	return coefficient
 
 #-------------------------------------------------------------------------------
 #-------------------------SIMULATION MAIN LOOP----------------------------------
@@ -56,15 +60,12 @@ while not converged and iteration < problemData.maxNumberOfIterations:
 		for region in grid.regions:
 			shearModulus = problemData.propertyData[region.handle]["ShearModulus"]
 			poissonsRatio = problemData.propertyData[region.handle]["PoissonsRatio"]
-			lamesConstant=2*shearModulus*poissonsRatio/(1-poissonsRatio)
-			
-			constitutiveMatrix = np.array([[2*shearModulus+lamesConstant,lamesConstant,0],[lamesConstant,2*shearModulus+lamesConstant,0],[0,0,shearModulus]] )
 			
 			for element in region.elements:
 				for innerFace in element.innerFaces:
 					backwardVertexHandle = element.vertices[element.shape.innerFaceNeighborVertices[innerFace.local][0]].handle
 					forwardVertexHandle = element.vertices[element.shape.innerFaceNeighborVertices[innerFace.local][1]].handle
-					coefficient = computeLocalMatrixCoefficient(innerFace, shearModulus, lamesConstant)
+					coefficient = computeLocalMatrixCoefficient(innerFace, shearModulus, poissonsRatio)
 
 					i=0
 					for vertex in element.vertices:
@@ -80,10 +81,15 @@ while not converged and iteration < problemData.maxNumberOfIterations:
 
 
 	# Neumann Boundary Condition
-	# for bCondition in problemData.neumannBoundaries["u"]:
-	# 	for facet in bCondition.boundary.facets:
-	# 		for outerFace in facet.outerFaces:
-	# 			independent[outerFace.vertex.handle] -= bCondition.getValue(outerFace.handle) * np.linalg.norm(outerFace.area.getCoordinates())
+	for bCondition in problemData.neumannBoundaries["u"]:
+		for facet in bCondition.boundary.facets:
+			for outerFace in facet.outerFaces:
+				independent[outerFace.vertex.handle] = bCondition.getValue(outerFace.handle) * np.linalg.norm(outerFace.area.getCoordinates())
+
+	for bCondition in problemData.neumannBoundaries["v"]:
+		for facet in bCondition.boundary.facets:
+			for outerFace in facet.outerFaces:
+				independent[outerFace.vertex.handle] = bCondition.getValue(outerFace.handle) * np.linalg.norm(outerFace.area.getCoordinates())
 
 	# Dirichlet Boundary Condition
 	for bCondition in problemData.dirichletBoundaries["u"]:
@@ -107,6 +113,19 @@ while not converged and iteration < problemData.maxNumberOfIterations:
 	#-------------------------SOLVE LINEAR SYSTEM-------------------------------
 	displacements = np.linalg.solve(matrix, independent)
 
+	normal_stain_x = np.zeros(numberOfVertices)
+	normal_stain_y = np.zeros(numberOfVertices)
+	for element in grid.elements:
+		for innerFace in element.innerFaces:
+			b = element.vertices[element.shape.innerFaceNeighborVertices[innerFace.local][0]].handle
+			f = element.vertices[element.shape.innerFaceNeighborVertices[innerFace.local][1]].handle
+
+			normal_stain_x[b] += np.dot( innerFace.globalDerivatives[0], np.array([displacements[vertex.handle] for vertex in element.vertices]) )
+			normal_stain_x[f] -= np.dot( innerFace.globalDerivatives[0], np.array([displacements[vertex.handle] for vertex in element.vertices]) )
+			normal_stain_y[b] += np.dot( innerFace.globalDerivatives[0], np.array([displacements[vertex.handle+numberOfVertices] for vertex in element.vertices]) )
+			normal_stain_y[f] -= np.dot( innerFace.globalDerivatives[0], np.array([displacements[vertex.handle+numberOfVertices] for vertex in element.vertices]) )
+
+
 	#-------------------------PRINT ITERATION DATA------------------------------
 	if iteration > 0:
 		print("{:>9}\t{:>14e}\t{:>14e}\t{:>14e}".format(iteration, currentTime, 0.0, difference))
@@ -115,6 +134,8 @@ while not converged and iteration < problemData.maxNumberOfIterations:
 	# ACTUALLY DISPLACEMENTS WILL BE ONE MATRIX
 	cgnsSaver.save('u', displacements[:numberOfVertices], 0.0)
 	cgnsSaver.save('v', displacements[numberOfVertices:], 0.0)
+	cgnsSaver.save('exx', normal_stain_x, 0.0)
+	cgnsSaver.save('eyy', normal_stain_y, 0.0)
 
 	#-------------------------CHECK CONVERGENCE---------------------------------
 	converged = False
@@ -141,10 +162,29 @@ from scipy.interpolate import griddata
 X,Y = zip(*[v.getCoordinates()[:-1] for v in grid.vertices])
 
 Xi, Yi = np.meshgrid( np.linspace(min(X), max(X), len(X)), np.linspace(min(Y), max(Y), len(Y)) )
-nTi = griddata((X,Y), displacements[:numberOfVertices], (Xi,Yi), method='linear')
 
-plt.pcolor(Xi,Yi,nTi, cmap=colors.ListedColormap( cm.get_cmap("RdBu",256)(np.linspace(1,0,256)) ))
-# plt.pcolor(Xi,Yi,nTi, cmap="RdBu")
-plt.title("Numerical Temperature")
-plt.colorbar()	
+u = griddata((X,Y), displacements[:numberOfVertices], (Xi,Yi), method='linear')
+plt.pcolor(Xi,Yi,u, cmap=colors.ListedColormap( cm.get_cmap("RdBu",256)(np.linspace(1,0,256)) ))
+plt.title("X displacement (u)")
+plt.colorbar()
+
+plt.figure()
+v = griddata((X,Y), displacements[numberOfVertices:], (Xi,Yi), method='linear')
+plt.pcolor(Xi,Yi,v, cmap=colors.ListedColormap( cm.get_cmap("RdBu",256)(np.linspace(1,0,256)) ))
+plt.title("Y displacement (v)")
+plt.colorbar()
+
+plt.figure()
+exx = griddata((X,Y), normal_stain_x, (Xi,Yi), method='linear')
+plt.pcolor(Xi,Yi,exx, cmap=colors.ListedColormap( cm.get_cmap("RdBu",256)(np.linspace(1,0,256)) ))
+plt.title("X normal strain (exx)")
+plt.colorbar()
+
+plt.figure()
+eyy = griddata((X,Y), normal_stain_y, (Xi,Yi), method='linear')
+plt.pcolor(Xi,Yi,eyy, cmap=colors.ListedColormap( cm.get_cmap("RdBu",256)(np.linspace(1,0,256)) ))
+plt.title("Y normal strain (eyy)")
+plt.colorbar()
+
+
 plt.show()

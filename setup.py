cdffe3f8230363a76003f1f8abd393aa8159405f
GustavoExel <gustavoexelgpe@gmail.com>
2022-01-14 00:18:31 -0300

    updated setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setup(
 	name="PyEFVLib",
-	version="1.0.7",
+	version="1.0.8",
 	description="A library that facilitates the implementation of the Element-based Finite Volumes Method (EbFVM) to solve partial differential equations (PDEs)",
 	py_modules=["PyEFVLib/__init__", "PyEFVLib/Vertex", "PyEFVLib/OuterFace", "PyEFVLib/GridData", "PyEFVLib/InnerFace", "PyEFVLib/Point", "PyEFVLib/Shape", "PyEFVLib/Region", "PyEFVLib/Facet", "PyEFVLib/MSHReader", "PyEFVLib/XDMFReader", "PyEFVLib/Boundary", "PyEFVLib/Element", "PyEFVLib/Grid", "PyEFVLib/BoundaryConditions", "PyEFVLib/Saver", "PyEFVLib/CsvSaver", "PyEFVLib/VtuSaver", "PyEFVLib/VtmSaver", "PyEFVLib/MeshioSaver", "PyEFVLib/ProblemData", "PyEFVLib/Solver", "PyEFVLib/LinearSystem"],
 	# package_dir={"": "PyEFVLib"},
@@ -20,15 +20,14 @@ setup(
 	long_description=long_description,
 	long_description_content_type="text/markdown",
 	install_requires=[
-		"numpy ~= 1.19.2",
-		"pandas ~= 1.1.2",
-		"scipy ~= 1.5.2",
+		"numpy ~= 1.22",
+		"pandas ~= 1.3",
+		"scipy ~= 1.7",
 		"meshio ~= 4.3.4",
-		"h5py ~= 3.2.1",
+		"h5py ~= 3.5",
 		"xmltodict ~= 0.12.0",
 	],
 	url="https://github.com/GustavoExel/PyEFVLib",
 	author="Gustavo Exel",
 	author_email="gustavoexelgpe@gmail.com",
-
 )
